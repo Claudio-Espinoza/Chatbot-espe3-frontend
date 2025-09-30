fcaefa09727bd679cc91d1a4ce59e28311e190b9
Claudio Espinoza <c.espinoza28@ufromail.cl>
2025-09-30 11:44:15 -0300

    feat: add ChatPage and update MetricPage with new content

---
+++ src/pages/ChatPage.tsx
@@ -0,0 +1,8 @@
+
+const ChatPage: React.FC = () => {
+    return (    
+    <h1>MetricPage</h1>
+    );
+}
+
+export default ChatPage
---
+++ src/pages/MetricPage.tsx
@@ -0,0 +1,8 @@
+
+const MetricPage: React.FC = () => {
+    return (
+    <h1>Sos</h1>
+    );
+}
+
+export default MetricPage
